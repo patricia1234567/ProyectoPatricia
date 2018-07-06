2686070e8fe49a19b57f492c113b6060648fb40e
patricia <pat.ravera@gmail.com>
2018-07-06 13:32:47 -0300

    Primer commit

---
+++ arrayPares/src/arraypares/ArrayPares.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arraypares;
+
+/**
+ *
+ * @author Paty OncaCor
+ */
+public class ArrayPares {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        //Carga de la Matriz
+        int array[][] = new int[3][3];
+        int contador = 0;
+        for (int fila = 0; fila < 3; fila++) {
+            for (int columna = 0; columna < 3; columna++) {
+                array[fila][columna] = contador;
+                contador = contador + 2;
+            }
+        }
+        //Mostrar
+        for (int fila = 0; fila < 3; fila++) {
+            for (int columna = 0; columna < 3; columna++) {
+                System.out.printf(" %d", array[fila][columna]);
+            }
+            System.out.printf(" \n");
+        }
+    }
+
+}
